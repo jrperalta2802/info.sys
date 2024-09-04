1c73edeb440fcec647848462d4d5f4304ca156e2
peralta28 <realinobperaltajr@iskolarngbayan.pup.edu.ph>
2024-09-04 12:58:02 +0800

    with Updated Database

--- infosys/db/database_folder/datasys.sql
+++ infosys/db/database_folder/datasys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2024 at 11:11 AM
+-- Generation Time: Sep 04, 2024 at 06:57 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -47,7 +47,7 @@ CREATE TABLE `leaders` (
 --
 
 INSERT INTO `leaders` (`id`, `barangay`, `contact_number`, `precint_no`, `full_name`, `birthdate`, `age`, `address`, `civil_status`, `sex`, `leaders_photo`, `UID`) VALUES
-(13, 'Bagbaguin', '0947-513-6907', '154E', 'Sofia Penaranda', '2024-08-01', 14, '121 Guijo St.', 'Single', 'Male', '2x2.jpg', 'UID0001');
+(17, 'Bagbaguin', '0947-513-6907', '154E', 'Sofia Penaranda', '2024-07-31', 22, '121 Guijo St.', 'Single', 'Male', '1x1.png', 'UID0001');
 
 --
 -- Triggers `leaders`
@@ -79,15 +79,15 @@ CREATE TABLE `members` (
   `member_precinct` varchar(255) NOT NULL,
   `member_photo` varchar(255) NOT NULL,
   `leader_id` int(11) NOT NULL,
-  `UID` varchar(9) NOT NULL
+  `UIDM` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`id`, `member_name`, `member_birthdate`, `member_contact`, `member_precinct`, `member_photo`, `leader_id`, `UID`) VALUES
-(99, 'Realino Peralta', '2024-08-01', '0947-513-6907', '154F', 'id.jpg', 13, 'UIDM0001');
+INSERT INTO `members` (`id`, `member_name`, `member_birthdate`, `member_contact`, `member_precinct`, `member_photo`, `leader_id`, `UIDM`) VALUES
+(111, 'Realino Peralta', '2024-08-01', '0947-513-6907', '154F', '', 17, 'UIDM0001');
 
 --
 -- Triggers `members`
@@ -96,11 +96,11 @@ DELIMITER $$
 CREATE TRIGGER `trg_generate_uid_members` BEFORE INSERT ON `members` FOR EACH ROW BEGIN
     DECLARE last_id INT;
 
-    -- Get the last number in the UID sequence for members
-    SELECT IFNULL(MAX(CAST(SUBSTRING(UID, 5, 4) AS UNSIGNED)), 0) INTO last_id FROM members;
+    -- Get the last number in the UIDM sequence for members
+    SELECT IFNULL(MAX(CAST(SUBSTRING(UIDM, 5, 4) AS UNSIGNED)), 0) INTO last_id FROM members;
 
     -- Set the new UID value for members
-    SET NEW.UID = CONCAT('UIDM', LPAD(last_id + 1, 4, '0'));
+    SET NEW.UIDM = CONCAT('UIDM', LPAD(last_id + 1, 4, '0'));
 END
 $$
 DELIMITER ;
@@ -125,7 +125,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `token`) VALUES
-(1, 'admin', '$2y$10$tWfIYCXmJC33ipc4UEC0ROMWLg1tuuCblAu1R4W.6LF78CDT.t/fG', 'admin@admin.com', 'admin', 'ea6c84e3413a97a89f4f672531748431603fa8f823d2d789e473686e59aabaa5'),
+(1, 'admin', '$2y$10$tWfIYCXmJC33ipc4UEC0ROMWLg1tuuCblAu1R4W.6LF78CDT.t/fG', 'admin@admin.com', 'admin', 'e6ed23080cfe01c7033e32bef9435684079ac6209ba6aeeaf8cc8370a9fc4ca5'),
 (3, 'AUBREY', '$2y$10$FMXQiIsYjSzcoL72yg6.BOVQYAxv5Q0RChcofjLGSII97x5NRDAhC', 'Zafiroleonardo@gmail.com', 'user', '65f5cdb84b76941e5cf3a958ebf2922f6a64870709df8a17a73fc20ee2a64e5d');
 
 --
@@ -144,7 +144,7 @@ ALTER TABLE `leaders`
 --
 ALTER TABLE `members`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UID` (`UID`);
+  ADD UNIQUE KEY `UID` (`UIDM`);
 
 --
 -- Indexes for table `users`
@@ -162,13 +162,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `leaders`
 --
 ALTER TABLE `leaders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
 
 --
 -- AUTO_INCREMENT for table `users`
